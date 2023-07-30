e62d108ffe0a328dba3dcb1f18750e9607cc2b93
Manuel <manuel@strenge.ch>
2023-07-30 16:22:19 +0000

    added launch script and divided message server

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -2,45 +2,21 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"net"
 
-	pb "github.com/TheBromo/goWebService/common/chat"
-	"google.golang.org/grpc"
-)
-
-const (
-	defaultName = "world"
+	receiver "github.com/TheBromo/goWebService/client/message"
 )
 
 var (
-	addr = flag.String("addr", "localhost:50051", "the address to connect to")
-	name = flag.String("name", defaultName, "Name to greet")
+	//addr = flag.String("addr", "localhost:50051", "the address to connect to")
 	port = flag.Int("port", 50051, "The server port")
 )
 
-type server struct {
-	pb.UnimplementedTerminalAppServiceServer
-}
-
-func (c *server) HandleMessage(e pb.TerminalAppService_HandleMessageServer) error {
-	return nil
-}
-
 func main() {
 	flag.Parse()
-	
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
 
-	s := grpc.NewServer()
-	pb.RegisterTerminalAppServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	log.Println("starting client")
+
+	receiver.Start(*port)
+
 }
--- client/message/message_receiver.go
+++ client/message/message_receiver.go
@@ -1,1 +1,37 @@
 package message
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/TheBromo/goWebService/common/chat"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedTerminalAppServiceServer
+}
+
+func (c *Server) HandleMessage(e pb.TerminalAppService_HandleMessageServer) error {
+	return nil
+}
+
+func Start(port int) {
+	log.Println("starting receiver")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		return
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTerminalAppServiceServer(s, &Server{})
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+		return
+	}
+}
