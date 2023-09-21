c852c2ce7e7494be8bf89f02fe2c99deb995bd64
Manuel <manuel@strenge.ch>
2023-09-21 18:56:02 +0000

    disconnected view

---
+++ client/terminalview/terminalview.go
@@ -0,0 +1,136 @@
+package terminalview
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	pb "github.com/TheBromo/gochat/common/chat"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type TickMsg time.Time
+
+var (
+	count = 0
+)
+
+type (
+	errMsg error
+)
+
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	ready       bool
+	err         error
+	input       chan pb.Message
+	output      chan pb.Message
+}
+
+// Send a message every second.
+func tickEvery() tea.Cmd {
+	return tea.Every(time.Second, func(t time.Time) tea.Msg {
+		return TickMsg(t)
+	})
+}
+
+func InitialModel(input chan pb.Message, output chan pb.Message) model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 280
+
+	ta.SetWidth(50)
+	ta.SetHeight(2)
+
+	// Remove cursor line styling
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+	ta.FocusedStyle.Base.Align(lipgloss.Bottom)
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(50, 10)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+		input:       input,
+		output:      output,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(tickEvery(), tea.EnterAltScreen, textarea.Blink)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+		}
+	case TickMsg:
+		count++
+		// Return your Every command again to loop.
+		m.messages = append(m.messages, m.senderStyle.Render("You: ")+"message "+strconv.Itoa(count))
+		m.viewport.SetContent(strings.Join(m.messages, "\n"))
+		m.viewport.GotoBottom()
+		return m, tickEvery()
+	case tea.WindowSizeMsg:
+		chatHeight := lipgloss.Height(m.textarea.View())
+		if !m.ready {
+			m.viewport = viewport.New(msg.Width, msg.Height-chatHeight)
+			m.textarea.SetWidth(msg.Width)
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.textarea.SetWidth(msg.Width)
+			m.viewport.Height = msg.Height - chatHeight
+		}
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
+}
